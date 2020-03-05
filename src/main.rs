8e563ebe50e2b60c3b10387b6601a94876d93a05
Óscar Carrasco <oxcabe@gmail.com>
2020-03-05 00:26:32 +0000

    Implement help menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn help() {
+    println!("Usage: c51902asm [OPTIONS]... [INFILE]...
+Assembler for the 8-bit c51902 architecture.
+
+-h, --help      shows this menu.
+-o, --outfile   specifies the name for the machine code file.
+                Default filename is \"a.out\".
+-v, --version   shows the version number.");
+}
+
 fn main() {
-    println!("Hello, world!");
+    help();
 }
