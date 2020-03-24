523bea1fde0a7199d99c88863a632ac239d53091
Oscar Carrasco <oxcabe@gmail.com>
2020-03-24 22:33:34 +0000

    Implement basic assembly pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,195 @@
 extern crate clap;
+extern crate libc51902;
+
+use std::char;
+use std::cmp::Ordering;
+use std::fs::{self, File};
+use std::io::{self, Write};
+use std::str::FromStr;
 
 use clap::{Arg, App};
-use std::fs;
-use std::io;
+use libc51902::Opcode;
 
-fn parse_machine_code(infile: &str, outfile: Option<&str>) -> Result<String, io::Error> {
+fn parse_machine_code(infile: &str, outfile: Option<&str>)
+    -> Result<String, io::Error> {
     let infile_content: String = fs::read_to_string(&infile)?;
-    println!("{}:\n{}", infile, infile_content);
+    let asm_vec: Vec<String> = infile_content.lines()
+        .filter_map(|x| {
+            if !x.is_empty() {
+                Some(String::from(x))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mut bin_vec: Vec<String> = gen_bin_instr(&asm_vec);
+
+    match bin_vec.len().cmp(&1024) {
+        Ordering::Less => {
+            while bin_vec.len() < 1024 {
+                bin_vec.push(String::from("0".repeat(16)));
+            }
+        },
+        Ordering::Greater => {
+            panic!("Too many instructions!");
+        },
+        _ => ()
+    }
+    add_underscore_padding(&mut bin_vec);
+
+    if let Some(out) = outfile {
+        let mut of = File::create(out)?;
+        writeln!(of, "{}", bin_vec.join("\n"))?;
+    }
 
     Ok(infile_content)
+}
+
+fn gen_bin_instr(asm_vec: &Vec<String>) -> Vec<String> {
+    let word_vec: Vec<Vec<String>> = asm_vec.iter()
+        .map(|x|
+            x.split_terminator(" ").map(|x| String::from(x))
+                .collect()
+        ).collect();
+
+    // println!("{:?}", word_vec);
+
+    let mut bin_vec: Vec<String> = Vec::new();
+
+    for line in word_vec {
+        let mut bin_instr = String::from("");
+        let mut word = line.iter();
+        let mnemonic_opt = Opcode::from_str(capitalize_str(word.next().unwrap())
+            .as_str());
+
+        // Ensures that the mnemonic opcode exists
+        if let Ok(mnemonic) = mnemonic_opt {
+            let d = mnemonic.to_binary().iter()
+            .map(|&c| char::from_digit(c.into(), 10).unwrap())
+            .collect::<String>();
+
+            bin_instr.push_str(&d);
+
+            match mnemonic {
+                // nop
+                Opcode::Nop => {
+                    bin_instr.push_str("0000000000");
+                },
+                // li Rd inm
+                Opcode::Li => {
+                    let register_id = get_reg_id(&mut word);
+                    if register_id < 16 {
+                        let inm_num = get_raw_num(&mut word);
+                        bin_instr.push_str(format!("{:08b}{:04b}", inm_num, register_id)
+                            .as_str());
+                    }
+                },
+                // jmp inm
+                Opcode::Jmp => {
+                    let jump_addr = get_raw_num(&mut word);
+                    if jump_addr < 1024 {
+                        bin_instr.push_str(format!("{:010b}", jump_addr).as_str());
+                    }
+                },
+                // jz inm
+                Opcode::Jz => {
+                    let jump_addr = get_raw_num(&mut word);
+                    if jump_addr < 1024 {
+                        bin_instr.push_str(format!("{:010b}", jump_addr).as_str());
+                    }
+                },
+                // jnz inm
+                Opcode::Jnz => {
+                    let jump_addr = get_raw_num(&mut word);
+                    if jump_addr < 1024 {
+                        bin_instr.push_str(format!("{:010b}", jump_addr).as_str());
+                    }
+                },
+                // mov Rd, Rs
+                Opcode::Mov => {
+                    let rd = get_reg_id(&mut word);
+                    let rs = get_reg_id(&mut word);
+                    bin_instr.push_str(format!("{:04b}{:04b}{:04b}",
+                        rs, 0, rd).as_str());
+                },
+                // not Rs, Rd
+                Opcode::Not => {
+                    let rd = get_reg_id(&mut word);
+                    let rs = get_reg_id(&mut word);
+                    bin_instr.push_str(format!("{:04b}{:04b}{:04b}",
+                        rs, 0, rd).as_str());
+                },
+                // add Rd, R1, R2
+                Opcode::Add => {
+                    let rd = get_reg_id(&mut word);
+                    let r1 = get_reg_id(&mut word);
+                    let r2 = get_reg_id(&mut word);
+                    bin_instr.push_str(format!("{:04b}{:04b}{:04b}",
+                        r1, r2, rd).as_str());
+                },
+                // sub Rd, R1, R2
+                Opcode::Sub => {
+                    let rd = get_reg_id(&mut word);
+                    let r1 = get_reg_id(&mut word);
+                    let r2 = get_reg_id(&mut word);
+                    bin_instr.push_str(format!("{:04b}{:04b}{:04b}",
+                        r1, r2, rd).as_str());
+                },
+                // and Rd, R1, R2
+                Opcode::And => {
+                    let rd = get_reg_id(&mut word);
+                    let r1 = get_reg_id(&mut word);
+                    let r2 = get_reg_id(&mut word);
+                    bin_instr.push_str(format!("{:04b}{:04b}{:04b}",
+                        r1, r2, rd).as_str());
+                },
+                // or Rd, R1, R2
+                Opcode::Or => {
+                    let rd = get_reg_id(&mut word);
+                    let r1 = get_reg_id(&mut word);
+                    let r2 = get_reg_id(&mut word);
+                    bin_instr.push_str(format!("{:04b}{:04b}{:04b}",
+                        r1, r2, rd).as_str());
+                },
+                _ => {
+                    panic!("Unimplemented instruction: {:?}", mnemonic);
+                }
+            }
+            // println!("{}", bin_instr);
+            bin_vec.push(bin_instr);
+        }
+    }
+
+    bin_vec
+}
+
+fn get_reg_id(word: &mut std::slice::Iter<'_, std::string::String>) -> u8 {
+    word.next().unwrap().split_terminator("R")
+        .collect::<String>().parse::<u8>().unwrap()
+}
+
+fn get_raw_num(word: &mut std::slice::Iter<'_, std::string::String>) -> u16 {
+    word.next().unwrap().parse::<u16>().unwrap()
+}
+
+fn add_underscore_padding(bin_instr: &mut Vec<String>) {
+    for line in bin_instr {
+        let initial_len = line.len();
+        let mut expansion = 0;
+        for pos in 1..initial_len / 4 {
+            line.insert(pos * 4 + expansion,   '_');
+            expansion += 1;
+        }
+    }
+}
+
+fn capitalize_str(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
 }
 
 fn main() -> Result<(), io::Error> {
